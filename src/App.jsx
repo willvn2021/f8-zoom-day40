eb148c045aff86293c3d2a32b5b6f0134fa8248d
F8 <williamlrasmussen@gmail.com>
2025-09-26 17:49:25 +0700

    Update from deploy script

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import "./App.css";
-import AppRoutes from "./componentS/AppRoutes";
-
-function App() {
-    return <AppRoutes />;
-}
-
-export default App;
--- src/pageS/DemoReduxCore/index.jsx
+++
@@ -1,5 +0,0 @@
-function DemoReduxCore() {
-    return <h1>DemoReduxCore</h1>;
-}
-
-export default DemoReduxCore;
